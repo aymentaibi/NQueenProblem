0baafa0656d6e81f5899dc59b27fcc95683fd833
aymentaibi <aymentaibi662@gmail.com>
2023-03-01 17:58:58 +0100

    BFS-done

--- ALGORITHME/src/Main.java
+++ ALGORITHME/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
+
     public static void main(String[] args) {
-        Problem prblm = new Problem(12);
-        prblm.SolveByDFS();
+        Problem prblm = new Problem(10);
+        prblm.SolveByBFS();
         if(prblm.isValide()) System.out.println("True");
         else System.out.println("False");
     }
--- ALGORITHME/src/Problem.java
+++ ALGORITHME/src/Problem.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem {
@@ -6,7 +7,6 @@ public class Problem {
     public Problem(int n) {
         this.cases = new int[n];
     }
-
     boolean isValide(){
         for(int row=0;row<this.cases.length;row++){
             int col = this.cases[row];
@@ -42,8 +42,6 @@ public class Problem {
         }
         return true;
     }
-
-
     void SolveByDFS(){
         dfsQueen dfsQueen= new dfsQueen(this.cases.length);
         dfsQueen.DFS(0,this,this.cases.length);
@@ -52,15 +50,22 @@ public class Problem {
             System.out.println("La ligne:" + i + " La Column:" + this.cases[i]);
         }
     }
+    void SolveByBFS(){
+        bfsQueen bfsQueen= new bfsQueen(this.cases.length);
+        bfsQueen.BFS(this,this.cases.length);
+        System.out.println("Le nombre de solution =" +bfsQueen.nbrSolution);
+        for(int i=0 ; i<this.cases.length;i++){
+            System.out.println("La ligne:" + i + " La Column:" + this.cases[i]);
+        }
+    }
 }
 class dfsQueen{
     List<Integer> tempList;
     int[] cases;
+    int nbrSolution = 0;
     public dfsQueen(int n) {
         cases = new int[n];
     }
-
-    int nbrSolution = 0;
     void DFS(int row,Problem prblm,int n){
         if(row == n){
             nbrSolution++;
@@ -104,5 +109,121 @@ class dfsQueen{
             if(isSafe) casePosiible.add(col);
         }
         return casePosiible;
+    }
+}
+class bfsQueen{
+    int nbrSolution = 0;
+    int[] cases;
+    public bfsQueen(int n) {
+        cases = new int[n];
+    }
+    void BFS(Problem problem, int n){
+        int row = 0,i=0;
+        noeud noeudCurrent;
+        noeud tempNeud = new noeud(n,row);
+        LinkedList<noeud> ListsOverts = new LinkedList<noeud>();
+        List<Integer> tempList;
+        tempList = casePossible(row);
+        for (Integer integer : tempList) {
+            ListsOverts.add(new noeud(n,0,integer,0));
+        }
+        while(ListsOverts.size() != 0){
+            noeudCurrent = ListsOverts.removeFirst();
+            if(noeudCurrent.row == n-1){
+                nbrSolution++;
+                if(nbrSolution == 1) {
+                    if (n >= 0) System.arraycopy(noeudCurrent.cases, 0, problem.cases, 0, n);
+                }
+            }
+            else {
+                //NeudCurrent
+                row = noeudCurrent.row + 1;
+                tempList = casePossible(row, noeudCurrent);
+                for (Integer integer : tempList) {
+                    i=0;
+                    for(i=0; i<row;i++){
+                        tempNeud.cases[i] = noeudCurrent.cases[i];
+                    }
+                    tempNeud.row = noeudCurrent.row + 1;
+                    tempNeud.cases[row] = integer;
+                    ListsOverts.add(new noeud(tempNeud));
+                }
+            }
+        }
+    }
+
+    List<Integer> casePossible(int row){
+        boolean isSafe = true;
+        List<Integer> casePosiible = new ArrayList<Integer>();
+        for(int col =0;col<this.cases.length; col++){
+            isSafe = true;
+            // Vérifier la diagonale sup adr
+            for (int i = row -1 , j = col+1; i >= 0 && j < this.cases.length && isSafe; i--, j++) {
+                if (this.cases[i] == j) {
+                    isSafe = false;
+                }
+            }
+            // Vérifier la diagonale sup fauch
+
+            for (int i = row -1 , j = col-1; i >= 0 && j >= 0 && isSafe; i--, j--) {
+                if (this.cases[i] == j) {
+                    isSafe = false;
+                }
+            }
+            // Vérifier la column
+
+            for(int i = 0;i<row && isSafe;i++){
+                if(this.cases[i] == col) isSafe = false;
+            }
+            //put in the liste
+            if(isSafe) casePosiible.add(col);
+        }
+        return casePosiible;
+    }
+    List<Integer> casePossible(int row, noeud inst){
+        boolean isSafe = true;
+        List<Integer> casePosiible = new ArrayList<Integer>();
+        for(int col =0;col<inst.cases.length; col++){
+            isSafe = true;
+            // Vérifier la diagonale sup adr
+            for (int i = row -1 , j = col+1; i >= 0 && j < inst.cases.length && isSafe; i--, j++) {
+                if (inst.cases[i] == j) {
+                    isSafe = false;
+                }
+            }
+            // Vérifier la diagonale sup fauch
+
+            for (int i = row -1 , j = col-1; i >= 0 && j >= 0 && isSafe; i--, j--) {
+                if (inst.cases[i] == j) {
+                    isSafe = false;
+                }
+            }
+            // Vérifier la column
+
+            for(int i = 0;i<row && isSafe;i++){
+                if(inst.cases[i] == col) isSafe = false;
+            }
+            //put in the liste
+            if(isSafe) casePosiible.add(col);
+        }
+        return casePosiible;
+    }
+}
+class noeud{
+    int[] cases;
+    int row;
+    public noeud(int n,int row) {
+        this.cases = new int[n];
+        this.row = row;
+    }
+    public noeud(noeud n){
+        this.row = n.row;
+        this.cases = new int[n.cases.length];
+        System.arraycopy(n.cases, 0, this.cases, 0, n.cases.length);
+    }
+    public noeud(int n,int row,int colm,int row2) {
+        this.cases = new int[n];
+        this.cases[row] = colm;
+        this.row = row2;
     }
 }
